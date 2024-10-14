96d88f62515bf06f7debc8db3b9a74cd74f11dce
Jake Kealey <jake.kealey@nhs.net>
2024-10-14 13:08:43 +0100

    Add: Initial file for the nhs workforce statistics pipeline

---
+++ src/utils/nhs_wf_stats.py
@@ -0,0 +1,12 @@
+'''
+Pipeline for outputs sourced from the NHS Workforce Statistics source data
+Source: https://digital.nhs.uk/data-and-information/publications/statistical/nhs-workforce-statistics/
+'''
+
+#Load the source data from the nwfs source files
+def load_nwfs_data():
+    pass
+
+def nhs_wf_stats():
+    #Load the data
+    df_nwfs = load_nwfs_data()
