d9bb823f08bc4a6553924cb7bb0b23e8bd042495
Jake Kealey <jake.kealey@nhs.net>
2024-10-14 13:25:29 +0100

    Edit: Sets runtime setting variables to a variable in the parent file

--- src/wf_ahp.py
+++ src/wf_ahp.py
@@ -1,10 +1,13 @@
 '''
 Script to execute the pipelines involved in the AHP Workforce Report generation. 
 '''
+from utils.runtime_settings import load_runtime_settings
+
 from utils.nhs_wf_stats import *
 
 # Load runtime settings
+settings = load_runtime_settings()
 
 # NHS Workforce Statistics Pipeline
 if True:
-    nhs_wf_stats()
+    nhs_wf_stats(settings=settings)
