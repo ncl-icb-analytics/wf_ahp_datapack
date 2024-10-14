73eb1e7160d0a448549cb54d0c521ba02df7a255
Jake Kealey <jake.kealey@nhs.net>
2024-10-14 15:38:20 +0100

    Edit: Added runtime parameters to specify organisations and front end names to use

--- src/utils/runtime_settings.py
+++ src/utils/runtime_settings.py
@@ -15,11 +15,19 @@ def load_runtime_settings():
     #These settings should remain unchanged and are combined with toml and env
     #settings to build the settings dict.
     base_path = "data/"
+    base_scope = "scope"
     base_pipeline_nwfs = "nhs_workforce_statistics"
 
     #Store both the config and env settings in a dict
     settings = {
-        "nwfs_path": base_path + config[base_pipeline_nwfs]["rel_path"]
+        "org_codes": config[base_scope]["org_codes"],
+        "org_shorts": config[base_scope]["org_shorts"],
+
+        "nwfs_path": base_path + config[base_pipeline_nwfs]["rel_path"],
+        "nwfs_sg2code": config[base_pipeline_nwfs]["sg2_code"],
+        "nwfs_colahp": config[base_pipeline_nwfs]["colname_ahp"],
+        "nwfs_colrole": config[base_pipeline_nwfs]["colname_role"],
+        "nwfs_colband": config[base_pipeline_nwfs]["colname_band"]
     }
 
     return settings
