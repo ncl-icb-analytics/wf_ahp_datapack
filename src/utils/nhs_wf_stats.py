10e3bf4db395336b00fd6bbe733f82e67e19fee8
Jake Kealey <jake.kealey@nhs.net>
2024-10-17 13:59:52 +0100

    Added plot_role_by_org function. Need to replace org_code with org_shorts and see if it works doing org by role

--- src/utils/nhs_wf_stats.py
+++ src/utils/nhs_wf_stats.py
@@ -157,6 +157,60 @@ def plot_role_by_band(df, settings):
     # Export the NCL data
     #df.to_csv("ncldata.csv", index=False)
 
+#Plot AHP Role against Organisation
+def plot_role_by_org (df, settings):
+
+    #Only consider latest data
+    df = df[df["period"] == df["period"].max()]
+
+    #Load list of AHP roles
+    ahp_roles = pd.read_csv(
+        "docs/nwfs_lookup.csv")["staff_role_frontend"].unique()
+    ahp_roles.sort()
+
+    # Initialize the figure and axes for a 4x3 grid
+    fig, axes = plt.subplots(3, 4, figsize=(16, 9))
+    axes = axes.flatten()
+
+    #Load list of orgs
+    org_codes = settings["org_codes"]
+    org_shorts = settings["org_shorts"]
+
+    # Loop over each axis and plot the barplots
+    for i, ax in enumerate(axes):
+        df_role = df.copy()[df["staff_role"] == ahp_roles[i]]
+
+        #Aggregate data
+        df_role = df_role.groupby('org_code', as_index=False)['wte'].sum()
+
+        sns.barplot(
+            x="org_code", 
+            y="wte", 
+            data=df_role,
+            ax=ax,
+            color="#242853",
+        )
+
+        #Format the x axis to be consistent for all graphs
+        ax.set_xticks(range(len(org_codes)))
+        ax.set_xticklabels(org_codes)
+
+        #Remove box from graphs
+        sns.despine()
+
+        # Format the titles and axes
+        ax.set_title(ahp_roles[i])
+        ax.set_xlabel('Provider')
+        ax.set_ylabel('WTE')
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    # Show the plot
+    #plt.show()
+
+    plt.suptitle("AHP Role WTE by Provider", fontsize=20)
+    plt.tight_layout(rect=[0, 0, 1, 0.98])
+    plt.savefig('./output/role_by_org/wte_by_afc_org.png', dpi=300, bbox_inches='tight')
+
 #Main function for the pipeline
 def nhs_wf_stats(settings):
 
@@ -166,4 +220,7 @@ def nhs_wf_stats(settings):
     #df_nwfs.to_csv("sample.csv", index=False)
 
     #Plot AHP Staff Role by Band
-    plot_role_by_band(df_nwfs, settings=settings)
+    #plot_role_by_band(df_nwfs, settings=settings)
+
+    #Plot AHP Staff Role by Organisation
+    plot_role_by_org (df_nwfs, settings)
