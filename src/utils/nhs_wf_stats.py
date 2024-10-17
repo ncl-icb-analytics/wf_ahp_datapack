42c988e8fd34574393b4421ceaf571631a4f81bd
Jake Kealey <jake.kealey@nhs.net>
2024-10-17 13:41:41 +0100

    Edit: Added function to trim AfC Band column so visual is more concise. Added function for plotting AHP Role by AfC Band

--- src/utils/nhs_wf_stats.py
+++ src/utils/nhs_wf_stats.py
@@ -5,6 +5,10 @@ Source: https://digital.nhs.uk/data-and-information/publications/statistical/nhs
 import os
 import pandas as pd
 
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+import seaborn as sns
+
 #Find all unique column values for a specified column that contains the target
 #I am fully aware this is not what fuzzy matching is
 def fuzzy_search(df_nwfs, col_name, target_string):
@@ -32,9 +36,15 @@ def nwfs_staff_role_fuzzy_mapping(df_nwfs, settings):
             sr_map[sr] = front_name
 
     #Apply the mapping
-    df_nwfs["mapped"] = df_nwfs["staff_role"].map(sr_map)
+    df_nwfs["staff_role"] = df_nwfs["staff_role"].map(sr_map)
     
     return df_nwfs
+
+def format_afc_col(val):
+    if val[0] == "N":
+        return "Non-AfC"
+    else:
+        return val.split(" ")[1]
 
 #Load the source data from the nwfs source files
 def load_nwfs_data(settings):
@@ -85,10 +95,75 @@ def load_nwfs_data(settings):
     ##Format staff role column using fuzzy function
     df_src = nwfs_staff_role_fuzzy_mapping(df_src, settings=settings)
 
-    print(df_src.head())
+    #Format the AfC Band column
+    df_src["afc_band"] = df_src["afc_band"].apply(format_afc_col)
+
+    return df_src
+
+#Plot AHP Role against Band
+def plot_role_by_band(df, settings):
+
+    #Only consider latest data
+    df = df[df["period"] == df["period"].max()]
+
+    #Load list of AHP roles
+    ahp_roles = pd.read_csv(
+        "docs/nwfs_lookup.csv")["staff_role_frontend"].unique()
+    ahp_roles.sort()
+
+    # Initialize the figure and axes for a 4x3 grid
+    fig, axes = plt.subplots(3, 4, figsize=(16, 9))
+    axes = axes.flatten()
+
+    # Get all bands from the data
+    afc_bands = df["afc_band"].unique()
+    afc_bands.sort()
+
+    # Loop over each axis and plot the barplots
+    for i, ax in enumerate(axes):
+        df_role = df.copy()[df["staff_role"] == ahp_roles[i]]
+
+        #Aggregate data
+        df_role = df_role.groupby('afc_band', as_index=False)['wte'].sum()
+
+        sns.barplot(
+            x="afc_band", 
+            y="wte", 
+            data=df_role,
+            ax=ax,
+            color="#242853",
+        )
+
+        #Format the x axis to be consistent for all graphs
+        ax.set_xticks(range(len(afc_bands)))
+        ax.set_xticklabels(afc_bands)
+
+        #Remove box from graphs
+        sns.despine()
+
+        # Format the titles and axes
+        ax.set_title(ahp_roles[i])
+        ax.set_xlabel('AFC Band')
+        ax.set_ylabel('WTE')
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    # Show the plot
+    #plt.show()
+
+    plt.suptitle("AHP Role WTE by AfC Band", fontsize=20)
+    plt.tight_layout(rect=[0, 0, 1, 0.98])
+    plt.savefig('./output/role_by_band/wte_by_afc_band.png', dpi=300, bbox_inches='tight')
+
+    # Export the NCL data
+    #df.to_csv("ncldata.csv", index=False)
 
 #Main function for the pipeline
 def nhs_wf_stats(settings):
 
     #Load the data
     df_nwfs = load_nwfs_data(settings=settings)
+
+    #df_nwfs.to_csv("sample.csv", index=False)
+
+    #Plot AHP Staff Role by Band
+    plot_role_by_band(df_nwfs, settings=settings)
