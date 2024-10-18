f71091d7b18677fefc112c13c1a4be272879b793
Jake Kealey <jake.kealey@nhs.net>
2024-10-18 14:42:15 +0100

    Added pwr_trends pipeline settings and generic SQL Address from the .env

--- src/utils/runtime_settings.py
+++ src/utils/runtime_settings.py
@@ -17,17 +17,20 @@ def load_runtime_settings():
     base_path = "data/"
     base_scope = "scope"
     base_pipeline_nwfs = "nhs_workforce_statistics"
+    base_pipeline_pwr = "pwr_trends"
 
     #Store both the config and env settings in a dict
     settings = {
         "org_codes": config[base_scope]["org_codes"],
         "org_shorts": config[base_scope]["org_shorts"],
+        "sql_address": getenv("SQL_ADDRESS"),
 
         "nwfs_path": base_path + config[base_pipeline_nwfs]["rel_path"],
-        "nwfs_sg2code": config[base_pipeline_nwfs]["sg2_code"],
         "nwfs_colahp": config[base_pipeline_nwfs]["colname_ahp"],
         "nwfs_colrole": config[base_pipeline_nwfs]["colname_role"],
-        "nwfs_colband": config[base_pipeline_nwfs]["colname_band"]
+        "nwfs_colband": config[base_pipeline_nwfs]["colname_band"],
+
+        "pwr_database": config[base_pipeline_pwr]["database"]
     }
 
     return settings
