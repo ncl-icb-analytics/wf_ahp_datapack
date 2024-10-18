1a6c88b2765ca6a0928743342b94d3fd3f13811d
Jake Kealey <jake.kealey@nhs.net>
2024-10-18 14:41:51 +0100

    Added pwr_trends to main

--- src/wf_ahp.py
+++ src/wf_ahp.py
@@ -4,10 +4,14 @@ Script to execute the pipelines involved in the AHP Workforce Report generation.
 from utils.runtime_settings import load_runtime_settings
 
 from utils.nhs_wf_stats import *
+from utils.pwr_trends import *
 
 # Load runtime settings
 settings = load_runtime_settings()
 
 # NHS Workforce Statistics Pipeline
 if True:
-    nhs_wf_stats(settings=settings)
+    #nhs_wf_stats(settings=settings)
+
+    pwr_trends(settings=settings)
+
